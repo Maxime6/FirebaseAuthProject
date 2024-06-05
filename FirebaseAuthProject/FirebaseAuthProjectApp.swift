6f59468379385c5b603fc59f1a378a92738a2d85
Max6me <maxime.tanter@hotmail.fr>
2024-06-05 16:10:44 +0200

    Initial Commit

---
+++ FirebaseAuthProject/FirebaseAuthProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthProjectApp.swift
+//  FirebaseAuthProject
+//
+//  Created by Maxime Tanter on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
